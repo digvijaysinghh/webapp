829a0f9856d2ed07636ee94662c0e27b8cda67f9
Digvijay Singh <106017750+digvijaysinghh@users.noreply.github.com>
2024-01-17 11:52:04 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Digvijay Singh",
+  title: "Cybersecurity Researcher",
+  email: "digvijay050203@gmail.com",
+  gitHub: "digvijaysinghh",
+  instagram: "digvijaysinghh_",
+  linkedIn: "digvijaysinghh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "digvijaysinghhh",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
